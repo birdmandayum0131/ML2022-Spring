01141d60b97c97f23bdfaa3e32e0ae83e644ba92
Bird Huang <bird.yz.huang@viewsonic.com>
2022-09-26 00:04:40 +0800

    try to use weight decay on model to improve model generalization ability (reduce overfitting) - not pass boss baseline yet

--- HW01/HW01_boss_baseline.py
+++ HW01/HW01_boss_baseline.py
@@ -148,7 +148,7 @@ def trainer(train_loader, valid_loader, model, config, device):
     # TODO: Please check https://pytorch.org/docs/stable/optim.html to get more available algorithms.
     # TODO: L2 regularization (optimizer(weight decay...) or implement by your self).
     optimizer = torch.optim.SGD(
-        model.parameters(), lr=config['learning_rate'], momentum=0.9)
+        model.parameters(), lr=config['learning_rate'], weight_decay=1e-7, momentum=0.9, )
 
     # writer = SummaryWriter() # Writer of tensoboard.
 
@@ -224,7 +224,7 @@ config = {
     'seed': 20040603,      # Your seed number, you can pick your lucky number. :)
     'select_all': False,   # Whether to use all features.
     'valid_ratio': 0.2,   # validation_size = train_size * valid_ratio
-    'n_epochs': 3000,     # Number of epochs.
+    'n_epochs': 10000,     # Number of epochs.
     'batch_size': 256,
     'learning_rate': 1e-5,
     # If model has not improved for this many consecutive epochs, stop training.
